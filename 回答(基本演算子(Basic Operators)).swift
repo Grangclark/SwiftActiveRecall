17b7b701acab5951e99daacb8db9556c1ed959c1
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-03-18 13:25:53 +0900

    Comparison Operators

---
+++ 回答(基本演算子(Basic Operators)).swift
@@ -0,0 +1,17 @@
+
+// # 回答(基本演算子(Basic Operators))
+
+// ## 基本演算子(Basic Operators)
+
+// ### 比較演算子(Comparison Operators)
+// ---
+
+// ** Q1. 比較演算子を使用した簡単なif文を記述して下さい **
+
+let name = "world"
+if name == "world" {
+    print("こんにちは, world")
+} else {
+    print("ごめんなさい \(name), あなただと気づきませんでした")
+}
+// name は "world" と等しいので、 こんにちは, world が出力される。
---
+++ 基本演算子(Basic Operators).md
@@ -0,0 +1,10 @@
+
+# Swift 学習ノート（Active Recall）
+<https://www.swiftlangjp.com/language-guide/basic-operators.html>
+--- 
+
+# 1. 基本演算子(Basic Operators)
+
+### 比較演算子(Comparison Operators)
+
+** Q1. 比較演算子を使用した簡単なif文を記述して下さい **
