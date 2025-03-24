081830c2fc26aea83e887a3df7cbfa57de886208
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-03-24 20:54:07 +0900

    Nil-Coalescing Operator

--- 回答(基本演算子(Basic Operators)).swift
+++ 回答(基本演算子(Basic Operators)).swift
@@ -15,3 +15,15 @@ if name == "world" {
     print("ごめんなさい \(name), あなただと気づきませんでした")
 }
 // name は "world" と等しいので、 こんにちは, world が出力される。
+
+
+// ### nil 合体演算子(Nil-Coalescing Operator)
+// ---
+
+// ** Q2. デフォルトのカラー名かユーザ定義のオプショナルなカラー名を選択するために、nil 合体演算子を使用して下さい。
+
+let defaultColorName = "red"
+var userDefinedColorName: String? // defaults は nil
+
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+// userDefinedColorName が nil なので、colorNameToUse にデフォルトの"red"が設定されます
